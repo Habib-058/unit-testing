0ff56c3e49d07c306fb58ac0ef7c903b132ea570
Habib_058 <habibur@nagorik.tech>
2025-12-26 23:52:31 +0600

    api handle unit testing

---
+++ lib/features/user/user_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:unit_testing/features/user/user_model.dart';
+import 'package:unit_testing/features/user/user_repository.dart';
+
+class UserPage extends StatefulWidget {
+  const UserPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<UserPage> createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  Future<User> getUsers = UserRepository(Client()).getUser();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: FutureBuilder(
+        future: getUsers,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return Center(
+            child: Text(
+              '${snapshot.data}',
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
